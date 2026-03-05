58e293a3b9657fa0c06ef0ecf60a63b7664866f4
Jose Salazar <joseasalazar83@gmail.com>
2026-03-05 11:25:42 -0600

    Refactor python azure function to explicit AsgiMiddleware to fix Azure Static Web Apps zero-functions validation issue

--- api/function_app.py
+++ api/function_app.py
@@ -1,4 +1,9 @@
 import azure.functions as func
 from main import app as fastapi_app
 
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="{*route}", auth_level=func.AuthLevel.ANONYMOUS)
+async def http_trigger(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(fastapi_app).handle_async(req, context)
+
